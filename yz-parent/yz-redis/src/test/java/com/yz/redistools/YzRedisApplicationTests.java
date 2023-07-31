ec507e65558409ec9b9b52d806e06d1d95f0b5b4
yunze <gaohan97@foxmail.com>
2023-08-01 00:06:56 +0800

    新版框架初始化搭建

---
+++ yz-parent/yz-redis/src/test/java/com/yz/redistools/YzRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.redistools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
