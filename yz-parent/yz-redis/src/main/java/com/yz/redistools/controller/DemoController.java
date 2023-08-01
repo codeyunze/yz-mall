c1b395e3fd34b6bb9d75ce1be74a46dd397ac419
yunze <gaohan97@foxmail.com>
2023-08-02 07:44:47 +0800

    redis哨兵架构使用案例

--- yz-parent/yz-redis/src/main/java/com/yz/redistools/controller/DemoController.java
+++ yz-parent/yz-redis/src/main/java/com/yz/redistools/controller/DemoController.java
@@ -1,5 +1,8 @@
 package com.yz.redistools.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +10,33 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yunze
  * @date 2023/7/31 0031 23:20
  */
+@Slf4j
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
     @RequestMapping("/redis")
-    public String redis(){
+    public String redis() {
         return "success!";
+    }
+
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping("/test_sentinel")
+    public void testSentinel() {
+        int i = 1;
+        while (true) {
+            try {
+                stringRedisTemplate.opsForValue().set("test-" + i, String.valueOf(i));
+                log.info("设置key：{}", "test-" + i);
+                i++;
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+                log.error("出现异常：{}", e.getMessage());
+            }
+        }
     }
 }
