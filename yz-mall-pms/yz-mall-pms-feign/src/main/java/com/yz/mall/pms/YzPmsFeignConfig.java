0f5797f12267928e3788b617e4b47786daa9a88b
yunze <gaohan97@foxmail.com>
2024-06-27 21:21:03 +0800

    fix: 遗漏文件补充

---
+++ yz-mall-pms/yz-mall-pms-feign/src/main/java/com/yz/mall/pms/YzPmsFeignConfig.java
@@ -0,0 +1,13 @@
+package com.yz.mall.pms;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yunze
+ * @date 2024/6/27 12:48
+ */
+@Configuration
+@EnableFeignClients
+public class YzPmsFeignConfig {
+}
