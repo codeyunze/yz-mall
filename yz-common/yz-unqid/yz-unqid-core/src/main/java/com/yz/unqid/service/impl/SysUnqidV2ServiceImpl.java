b8780fe5fb4a80e583fb5e81b1e57720dc874399
yunze <gaohan97@foxmail.com>
2024-07-10 21:47:10 +0800

    perf: 优化序列号生成

--- yz-common/yz-unqid/yz-unqid-core/src/main/java/com/yz/unqid/service/impl/SysUnqidV2ServiceImpl.java
+++ yz-common/yz-unqid/yz-unqid-core/src/main/java/com/yz/unqid/service/impl/SysUnqidV2ServiceImpl.java
@@ -2,13 +2,15 @@ package com.yz.unqid.service.impl;
 
 import cn.hutool.core.util.IdUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yz.advice.exception.BusinessException;
 import com.yz.tools.RedissonLockKey;
 import com.yz.unqid.entity.SysUnqid;
 import org.redisson.api.RLock;
-import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
@@ -32,41 +34,66 @@ public class SysUnqidV2ServiceImpl extends SysUnqidServiceImpl {
         redissonLock.lock(10, TimeUnit.SECONDS);
 
         try {
-            BoundHashOperations<String, Object, Object> boundHashOps = redisTemplate.boundHashOps(RedissonLockKey.objUnqid(prefix));
-            Object obj = boundHashOps.get(prefix);
-            SysUnqid bo;
-            if (obj == null) {
-                bo = baseMapper.selectOne(new LambdaQueryWrapper<SysUnqid>().eq(SysUnqid::getPrefix, prefix));
-                if (bo != null) {
-                    boundHashOps.put(prefix, bo);
-                }
-            } else {
-                bo = (SysUnqid) obj;
-            }
+            // 从缓存获取序列号对象信息
+            SysUnqid bo = getSysUnqidPriorityCache(prefix);
 
-            boolean saved;
-            if (bo == null) {
-                bo = new SysUnqid();
-                bo.setId(IdUtil.getSnowflakeNextIdStr());
-                bo.setSerialNumber(1);
-                bo.setPrefix(prefix);
-                saved = baseMapper.insert(bo) > 0;
-            } else {
-                bo.setSerialNumber(bo.getSerialNumber() + 1);
-                saved = baseMapper.updateById(bo) > 0;
-            }
-            boundHashOps.put(prefix, bo);
+            // 获取序列号的最新流水号，并更新缓存信息
+            Integer serialNumber = updateSysUnqidCache(prefix, bo);
 
-            if (!saved) {
+            if (serialNumber == null) {
                 throw new BusinessException(prefix + "流水号生成失败");
             }
 
-            String code = generateProcessor(prefix, numberLength, bo.getSerialNumber());
+            String code = generateProcessor(prefix, numberLength, serialNumber);
             // baseMapper.record(code);
             return code;
         } finally {
             redissonLock.unlock();
         }
+    }
+
+    /**
+     * 更新缓存的序列号对象信息
+     *
+     * @param prefix 序列号前缀
+     * @param bo     序列号对象信息
+     */
+    private Integer updateSysUnqidCache(String prefix, SysUnqid bo) {
+        if (bo == null) {
+            bo = new SysUnqid();
+            bo.setId(IdUtil.getSnowflakeNextIdStr());
+            bo.setSerialNumber(1);
+            bo.setPrefix(prefix);
+        } else {
+            bo.setSerialNumber(bo.getSerialNumber() + 1);
+        }
+        redisTemplate.boundHashOps(RedissonLockKey.objUnqid(prefix)).put(prefix, bo);
+        return bo.getSerialNumber();
+    }
+
+    /**
+     * 获取指定前缀的序列号对象信息-优先从缓存获取数据
+     *
+     * @param prefix 序列号前缀
+     * @return 序列号前缀对应的序列号对象信息
+     */
+    private SysUnqid getSysUnqidPriorityCache(String prefix) {
+        Object obj = redisTemplate.boundHashOps(RedissonLockKey.objUnqid(prefix)).get(prefix);
+        SysUnqid bo;
+        if (obj == null) {
+            bo = baseMapper.selectOne(new LambdaQueryWrapper<SysUnqid>().eq(SysUnqid::getPrefix, prefix));
+            if (bo != null) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+                    redisTemplate.boundHashOps(RedissonLockKey.objUnqid(prefix)).put(prefix, objectMapper.writeValueAsString(bo));
+                } catch (JsonProcessingException e) {
+                    throw new BusinessException(e.getMessage());
+                }
+            }
+        } else {
+            bo = (SysUnqid) obj;
+        }
+        return bo;
     }
 }
 
