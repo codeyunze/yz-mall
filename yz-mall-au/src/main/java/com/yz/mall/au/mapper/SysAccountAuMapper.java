df986099b2df62e5477c8ee563c9e8d17cd46c0d
yunze <gaohan97@foxmail.com>
2025-01-14 13:00:40 +0800

    perf: 交易汇总信息查询优化

--- yz-mall-au/src/main/java/com/yz/mall/au/mapper/SysAccountAuMapper.java
+++ yz-mall-au/src/main/java/com/yz/mall/au/mapper/SysAccountAuMapper.java
@@ -2,8 +2,10 @@ package com.yz.mall.au.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yz.mall.au.dto.SysAccountAuChooseQueryDto;
 import com.yz.mall.au.dto.SysAccountAuQueryDto;
 import com.yz.mall.au.entity.SysAccountAu;
+import com.yz.mall.au.vo.SysAccountAuChooseVo;
 import com.yz.mall.au.vo.SysAccountAuVo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -25,5 +27,14 @@ public interface SysAccountAuMapper extends BaseMapper<SysAccountAu> {
      * @return 分页列表数据
      */
     Page<SysAccountAuVo> selectPageByFilter(Page<Object> page, @Param("filter") SysAccountAuQueryDto filter);
+
+    /**
+     * 分页查询
+     *
+     * @param page   分页
+     * @param filter 过滤条件
+     * @return 分页列表数据
+     */
+    Page<SysAccountAuChooseVo> selectChoosePageByFilter(Page<Object> page, @Param("filter") SysAccountAuChooseQueryDto filter);
 }
 
