2aacccc5d94f900d92c53ced6fb3de3ffe112c03
yunze <gaohan97@foxmail.com>
2025-01-23 17:27:13 +0800

    perf: 商品展示信息开发完善

--- yz-mall-pms/yz-mall-pms-service/src/main/java/com/yz/mall/pms/mapper/PmsProductMapper.java
+++ yz-mall-pms/yz-mall-pms-service/src/main/java/com/yz/mall/pms/mapper/PmsProductMapper.java
@@ -1,8 +1,13 @@
 package com.yz.mall.pms.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yz.mall.pms.dto.PmsProductSlimQueryDto;
 import com.yz.mall.pms.entity.PmsProduct;
+import com.yz.mall.pms.vo.PmsProductDisplayInfoVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 商品表(PmsProduct)表数据库访问层
@@ -13,5 +18,12 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface PmsProductMapper extends BaseMapper<PmsProduct> {
 
+    /**
+     * 查询商品展示信息
+     *
+     * @param filter 数据过滤条件
+     * @return 商品展示信息
+     */
+    List<PmsProductDisplayInfoVo> selectProductDisplayInfoList(@Param("filter") PmsProductSlimQueryDto filter);
 }
 
