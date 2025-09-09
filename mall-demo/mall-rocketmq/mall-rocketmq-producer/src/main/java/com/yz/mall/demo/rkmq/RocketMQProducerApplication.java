a6fe8ea427396533f98a6c1560ad1624783c693d
yunze <gaohan97@foxmail.com>
2025-09-09 20:07:31 +0800

    feat: 新增mq消费者和生产者代码demo

---
+++ mall-demo/mall-rocketmq/mall-rocketmq-producer/src/main/java/com/yz/mall/demo/rkmq/RocketMQProducerApplication.java
@@ -0,0 +1,16 @@
+package com.yz.mall.demo.rkmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yunze
+ * @since 2025/9/7 14:06
+ */
+@SpringBootApplication
+public class RocketMQProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMQProducerApplication.class, args);
+    }
+}
