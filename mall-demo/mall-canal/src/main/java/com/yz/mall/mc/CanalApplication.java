36fb8d730e5e500217a742588ed568e55b12d2fa
yunze <834363368@qq.com>
2025-12-03 00:12:39 +0800

    perf: 集成calal客户端

---
+++ mall-demo/mall-canal/src/main/java/com/yz/mall/mc/CanalApplication.java
@@ -0,0 +1,16 @@
+package com.yz.mall.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yunze
+ * @since 2025/11/2 23:37
+ */
+@SpringBootApplication
+public class CanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+}
